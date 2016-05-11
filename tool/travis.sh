c5dbb8d388364db22b75fb10552d14513b7e67cf
Stéphane Este-Gracias <sestegra@gmail.com>
2016-05-11 13:58:08 +0200

    Add Travis CI & Coveralls configurations

---
+++ tool/travis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Fast fail the script on failures.
+set -e
+
+# Run vm tests
+pub run test -p vm
+
+# Run browser tests
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+pub serve &
+while ! nc -z localhost 8080; do sleep 1; done; echo 'pub serve is up!'
+pub run test --pub-serve=8080 -p content-shell -p firefox
+
+# Install dart_coveralls
+# Gather and send coverage data.
+if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --exclude-test-files \
+    test/serializer_test.dart
+fi
